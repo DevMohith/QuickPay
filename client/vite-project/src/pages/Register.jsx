b4348aeec5801ba75507149d186e4570b50fd275
Mohith Tummala <137559580+DevMohith@users.noreply.github.com>
2025-04-05 00:42:30 +0200

    developed Login and Register pages

---
+++ client/vite-project/src/pages/Register.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function Register() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleRegister = async() => {
+        const res = await axios.post("http://localhost:5000/api/auth/login",{
+            email,
+            password,
+        });
+        console.log("Register response", res.data);
+    };
+
+    return(
+        <div>
+        <h1>Register</h1>
+        <input placeholder="email" onChange={(e) => setEmail(e.target.value)}/>
+        <input placeholder="password" type="password" onChange={(e) => setPassword(e.target.value)}/>
+        <button onClick={handleRegister}>Registe</button>
+        </div>
+    );
+}
