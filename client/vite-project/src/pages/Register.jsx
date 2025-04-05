1e37ca278f56174b6d433eab715a6c2975af3a09
Mohith Tummala <137559580+DevMohith@users.noreply.github.com>
2025-04-05 02:28:37 +0200

    HTML fixes

--- client/vite-project/src/pages/Register.jsx
+++ client/vite-project/src/pages/Register.jsx
@@ -18,7 +18,7 @@ export default function Register() {
         <h1>Register</h1>
         <input placeholder="email" onChange={(e) => setEmail(e.target.value)}/>
         <input placeholder="password" type="password" onChange={(e) => setPassword(e.target.value)}/>
-        <button onClick={handleRegister}>Registe</button>
+        <button onClick={handleRegister}>Register</button>
         </div>
     );
 }
